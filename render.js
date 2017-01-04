33b1313854da06275bb2b82d5f533625d199655d
Jim Schofield <oldcoyote@gmail.com>
2017-01-04 12:12:47 -0600

    Lorem text now populates

--- render.js
+++ render.js
@@ -4,8 +4,22 @@ const encodingType = "{'encoding': 'utf8'}";
 
 function view(numberOfCharacters, res) {
 	var siteContent = fs.readFileSync('index.html');
+
+	//place to merge content
+	siteContent += selectLorem(20);
+
 	siteContent += fs.readFileSync('footer.html');
   	res.write(siteContent, res);
+}
+
+function selectLorem(numberOfCharacters) {
+	var lorem = fs.readFileSync('lorem.txt', 'utf8');
+
+	console.log(lorem);
+
+	lorem = lorem.slice(0,numberOfCharacters);
+
+	return lorem;
 }
 
 module.exports.view=view;
