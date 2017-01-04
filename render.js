40ecbe54f5167265fc723ac8248bf2e95b2fa187
Jim Schofield <oldcoyote@gmail.com>
2017-01-04 12:39:02 -0600

    lorem is now merged in doc

--- render.js
+++ render.js
@@ -5,8 +5,12 @@ const encodingType = "{'encoding': 'utf8'}";
 function view(numberOfCharacters, res) {
 	var siteContent = fs.readFileSync('index.html');
 
+	siteContent += "<div class='loremText'>";
+
 	//place to merge content
-	siteContent += selectLorem(20);
+	siteContent += selectLorem(numberOfCharacters);
+
+	siteContent += "</div>";
 
 	siteContent += fs.readFileSync('footer.html');
   	res.write(siteContent, res);
@@ -15,9 +19,9 @@ function view(numberOfCharacters, res) {
 function selectLorem(numberOfCharacters) {
 	var lorem = fs.readFileSync('lorem.txt', 'utf8');
 
-	console.log(lorem);
+	lorem = lorem.slice(0, numberOfCharacters);
 
-	lorem = lorem.slice(0,numberOfCharacters);
+	lorem.replace(/[\n\r]/g, '<br>')
 
 	return lorem;
 }
--- router.js
+++ router.js
@@ -4,7 +4,7 @@ var render = require("./render.js");
 function home(req, res) {
   res.statusCode = 200;
   res.writeHead(200, {'Content-Type': 'text/html'});
-  render.view(12, res);
+  render.view(1000, res);
   res.end();
 }
 
